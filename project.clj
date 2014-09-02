ed3a93ab02b605b5c2e5649bc07244c775e805e7
Paul Sabda <sabdaps@gmail.com>
2014-09-03 04:58:41 +0700

    fixed github url

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject org.clojars.squest/squest.questdb "0.1.0-SNAPSHOT"
   :description "A lightweight implementation of file-persisted couch-like embedded
   nosql database using edn only data structure"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/squest/questdb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
